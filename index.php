d88999c12fd20a55691cdeb4c410798d756f4816
Leo <leo3110.souza@gmail.com>
2021-12-21 18:17:54 -0300

    a

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="../script/css.css">
+    <title></title>
+  </head>
+  <body>
+
+    <div class="Topo">
+      <div class="Item">
+        a
+      </div>
+      <div class="Item">
+        a
+      </div>
+    </div>
+    <div class="💪">
+      <div class="LeftMenu">
+
+      </div>
+      <div class="Corpo">
+        <div class="Grid">
+
+          <div class="quadradao">
+
+          </div>
+
+          <div class="quadradao">
+
+          </div>
+
+          <div class="quadradao">
+
+          </div>
+
+          <div class="quadradao">
+
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+
+  </body>
+</html>
